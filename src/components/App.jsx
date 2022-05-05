6a5277a7b116e3d0a0b652c0921d7645fe1ebae0
Roman <77742141+rumpear@users.noreply.github.com>
2022-05-05 19:41:44 +0300

    add jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,105 @@
+import { nanoid } from 'nanoid';
+import styled from 'styled-components';
+
+import { PureComponent } from 'react';
+
+import { Container } from './Container';
+import { Section } from './Section';
+import { ContactsFilter } from './ContactsFilter';
+import { ContactsForm } from './ContactsForm';
+import { ContactsList } from './ContactsList';
+
+export class App extends PureComponent {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleAddContact = data => {
+    const { name, number } = data;
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(({ contacts }) => ({ contacts: [contact, ...contacts] }));
+  };
+
+  handleSearchContact = e => {
+    console.log(e.currentTarget.value);
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  handleFilterContact = () => {
+    const { contacts, filter } = this.state;
+    const normalizeFilterValue = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizeFilterValue),
+    );
+  };
+
+  handleDeleteContact = todoId => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== todoId),
+    }));
+  };
+
+  render() {
+    const {
+      handleAddContact,
+      handleSearchContact,
+      handleFilterContact,
+      handleDeleteContact,
+    } = this;
+    const { filter } = this.state;
+    const displayedContacts = handleFilterContact();
+
+    return (
+      <Section>
+        <Wrapper>
+          <Title>Phonebook</Title>
+          <ContactsForm
+            onAddContact={handleAddContact}
+            contacts={displayedContacts}
+          />
+          <ContactsFilter
+            value={filter}
+            onSearchContact={handleSearchContact}
+          />
+          <h2>Contacts</h2>
+          <ContactsList
+            contacts={displayedContacts}
+            onDeleteContact={handleDeleteContact}
+          />
+        </Wrapper>
+      </Section>
+    );
+  }
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  font-family: 'Poppins';
+
+  padding: 40px;
+  width: 400px;
+  margin: 100px auto;
+  background-color: rgba(151, 151, 151, 0.1);
+  border-radius: 5px;
+  box-shadow: rgba(99, 99, 99, 0.322) 0px 2px 8px 0px;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 15px;
+  font-size: 35px;
+`;
